249c97053cc6033a1ed4fdf3e0c39de5be3506d8
CarlosColnaghi <carlos.colnaghi@hotmail.com>
2026-05-30 17:29:56 -0300

    feat: implementing average calculation function

--- components/MediaCalculator.js
+++ components/MediaCalculator.js
@@ -1,30 +1,69 @@
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 function MediaCalculator(props){
+    const [getForm,  setForm] = useState({
+        name: "",
+        email: "",
+        grade1: "",
+        grade2: "",
+        grade3: "",
+    });
+
+    function updateForm(field, value){
+        setForm(form => ({...form, [field]: value}));
+    }
+
+    const [showResponse, setShowResponse] = useState(false);
+
+    const [getAverage, setAverage] = useState(0.0);
+
+    function calculateAverage(values){
+        return values.reduce((accumulator, value) => {return accumulator + Number(value)}, 0)/values.length;
+    }
+
+    function calculateAverageGrades(){
+        setAverage(calculateAverage([getForm.grade1, getForm.grade2, getForm.grade3]))
+        setShowResponse(true)
+    }
+
+    function restartForm(){
+        setForm({
+            name: "",
+            email: "",
+            grade1: "",
+            grade2: "",
+            grade3: "",
+        });
+        setShowResponse(false)
+    }
+
     return(
         <View style={styles.containers}>
             <Text style={styles.titles}>Calculadora de Média</Text>
-            <TextInput placeholder="Nome" style={styles.fields}/>
-            <TextInput placeholder="E-mail" style={styles.fields}/>
+            <TextInput placeholder="Nome" style={styles.fields} value={getForm.name} onChangeText={(text) => updateForm("name", text)}/>
+            <TextInput placeholder="E-mail" style={styles.fields} value={getForm.email} onChangeText={(text) => updateForm("email", text)}/>
             <View style={styles.gradeContainers}>
-                <TextInput placeholder="Nota 1" style={[styles.fields, styles.gradeFields]}/>
-                <TextInput placeholder="Nota 2" style={[styles.fields, styles.gradeFields]}/>
-                <TextInput placeholder="Nota 3" style={[styles.fields, styles.gradeFields]}/>
+                <TextInput placeholder="Nota 1" style={[styles.fields, styles.gradeFields]} value={getForm.grade1} onChangeText={(text) => updateForm("grade1", text)}/>
+                <TextInput placeholder="Nota 2" style={[styles.fields, styles.gradeFields]} value={getForm.grade2} onChangeText={(text) => updateForm("grade2", text)}/>
+                <TextInput placeholder="Nota 3" style={[styles.fields, styles.gradeFields]} value={getForm.grade3} onChangeText={(text) => updateForm("grade3", text)}/>
             </View>
             <View style={styles.buttonContainers}>
                 <View style={styles.buttons}>
-                    <Button title="Calcular"></Button>
+                    <Button title="Calcular" onPress={calculateAverageGrades}></Button>
                 </View>
                 <View style={styles.buttons}>
-                    <Button title="Reiniciar" color="#ff0000"></Button>
+                    <Button title="Reiniciar" color="#ff0000" onPress={restartForm}></Button>
                 </View>
             </View>
-            <View style={styles.responseContainers}>
-                <Text>Nome: </Text>
-                <Text>E-mail: </Text>
-                <Text>Notas: </Text>
-                <Text>Média: </Text>
-            </View>
+            {showResponse && (
+                <View style={styles.responseContainers}>
+                    <Text>Nome: {getForm.name}</Text>
+                    <Text>E-mail: {getForm.email}</Text>
+                    <Text>Notas: {getForm.grade1}, {getForm.grade2}, {getForm.grade3}</Text>
+                    <Text>Média: {getAverage.toFixed(2)}</Text>
+                </View>
+            )}
         </View>
     );
 }
